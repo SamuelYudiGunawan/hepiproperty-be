e177e049df0739b2c95f336991e3661e27d5b007
DaffaAhmadSM <daffamubaroqi321@gmail.com>
2023-11-07 18:34:01 +0700

    install spatie and add seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +18,41 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $role = [
+            [
+                'name' => 'Owner',
+                'guard_name' => 'web',
+            ],
+            [
+                'name' => 'Admin',
+                'guard_name' => 'web',
+            ],
+            [
+                'name' => 'Customer',
+                'guard_name' => 'web',
+            ],
+            [
+                'name' => 'Agent',
+                'guard_name' => 'web',
+            ],
+            [
+                'name' => 'PR',
+                'guard_name' => 'web',
+            ]
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        ];
+        for ($i=0; $i < count($role); $i++) { 
+            Role::create($role[$i]);
+        }
+        $user =  User::create(
+                [
+                    'name'     => 'owner',
+                    'email'    => 'owner@admin.com',
+                    'password' => Hash::make('owner1234'),
+                ]
+            );
+        $user = User::find(1);
+        $user -> assignRole('Owner');
+        
     }
 }
