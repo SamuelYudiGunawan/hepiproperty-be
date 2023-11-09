db1af35cff277216321251335c7efb4a5f68fc61
DaffaAhmadSM <daffamubaroqi321@gmail.com>
2023-11-09 08:18:33 +0700

    add migration kota and refactor model listing to property

--- database/migrations/2023_08_06_121019_create_property_table.php
+++ database/migrations/2023_08_06_121019_create_property_table.php
@@ -13,27 +13,27 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('listings', function (Blueprint $table) {
+        Schema::create('property', function (Blueprint $table) {
             $table->id();
             $table->string('judul');
             $table->string('tipe');
             $table->string('tipe_bangunan');
-            $table->string('harga');
-            $table->string('deskripsi');
+            $table->integer('harga');
+            $table->text('deskripsi');
             $table->string('area');
-            $table->integer('luas_tanah');
-            $table->integer('luas_bangunan');
             $table->integer('kamar_tidur');
             $table->integer('kamar_mandi');
+            $table->integer('luas_tanah');
+            $table->integer('luas_bangunan');
             $table->integer('kamar_tidur_pembantu');
             $table->integer('kamar_mandi_pembantu');
             $table->string('listrik');
             $table->string('air');
             $table->string('sertifikat');
-            $table->string('hadap');
+            $table->string('posisi_rumah');
             $table->boolean('garasi_dan_carport');
-            $table->string('furnished');
-            $table->foreignId('user_id')->constrained('users'); // Replace 'users' with the actual table name for the users.
+            $table->string('kondisi_bangunan');
+            $table->foreignId('agent_id')->constrained('users'); // Replace 'users' with the actual table name for the users.
             $table->timestamps();
         });
     }
@@ -45,6 +45,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('listings');
+        Schema::dropIfExists('property');
     }
 };
---
+++ database/seeders/KecamatanSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class KecamatanSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $sql = file_get_contents(database_path('seeders/sql/dt_kecamatan.sql'));
+        DB::unprepared($sql);
+    }
+}
